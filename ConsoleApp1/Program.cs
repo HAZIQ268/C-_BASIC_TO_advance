0f10a909d99c35ad044d01f670392f89353c1e15
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-10-11 18:48:26 +0500

    First Class

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace my_app {
+
+    class program { 
+    static void Main() {
+
+
+            Console.WriteLine("Enter Your Id");
+            // Convert integer variable to string
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            // First Method interpulation
+            Console.WriteLine($"id is : {id} \n name is : {name}");
+            // Second method concatenate method 
+            Console.WriteLine("id is :" + id + "\n" + "name is :" + name);
+        }
+    }
+
+}
