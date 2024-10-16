1fbf80ac9c2388ce2de5a24971d7be90c669044e
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-10-16 18:25:22 +0500

    second class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,41 @@
 ﻿namespace my_app {
 
-    class program { 
+    class program {
+
+        static List<string> list_name = new List<string>(); 
     static void Main() {
+            while (true)
+            {
+                Console.WriteLine("1.Add Student \n 2.Show Student");
+                int user_input = Convert.ToInt32 (Console.ReadLine());
+
+                if (user_input == 1)
+                {
+                    add_data();
+                }
+                else
+                {
+                    show_data();
+
+                }
 
 
-            Console.WriteLine("Enter Your Id");
-            // Convert integer variable to string
-            int id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Your Name");
-            string name = Console.ReadLine();
-            // First Method interpulation
-            Console.WriteLine($"id is : {id} \n name is : {name}");
-            // Second method concatenate method 
-            Console.WriteLine("id is :" + id + "\n" + "name is :" + name);
+            }
+
         }
+        static void add_data() { 
+                Console.WriteLine("Enter Your Name");
+                string name = Console.ReadLine();
+                list_name.Add(name);
+
+        }
+        static void show_data() {
+            foreach (string names in list_name)
+            {
+                Console.WriteLine($"name is {names}");
+            }
+        }
+
     }
 
 }
